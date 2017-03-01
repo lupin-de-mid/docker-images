2b98998141fd43f9b185c4e2a8d234fafe02c0da
lupin-de-mid <lupin@demid.su>
2017-03-02 02:52:25 +0300

    Update to 5.5.2

--- unity3d/get-unity.sh
+++ unity3d/get-unity.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-URL=http://download.unity3d.com/download_unity/linux/
-PKG=unity-editor-5.3.4f1+20160316_amd64.deb
+URL=http://beta.unity3d.com/download/e06241adb51f/unity-editor_amd64-5.5.2xf1Linux.deb
 
 echo "Downloading Unity3D installer..."
-curl -o /app/unity_editor.deb -s "${URL}${PKG}" 
+curl -o /app/unity_editor.deb -s "${URL}" 
 echo "Unity3D installer downloaded."
